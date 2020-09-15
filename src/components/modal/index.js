3041c8a9069e53b91ed725fb820888caa1ab7a82
Leonardo Garruba <leonardogarrubaaraujo@gmail.com>
2020-09-15 16:22:37 -0300

    Alteração de layout do Modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -100,13 +100,14 @@ export class ModalForm extends Component {
                                         </Form.Control>
                                     </Form.Group>
 
-                                    <Form.Group as={Col} controlId="Ip">
-                                        <Form.Label>IP</Form.Label>
+                                    <Form.Group as={Col} controlId="Torre">
+                                        <Form.Label>Torre</Form.Label>
+
                                         <Form.Control
                                             type="text"
-                                            name="ip"
+                                            name="torre"
                                             required
-                                            defaultValue={model.ip}
+                                            defaultValue={model.dominio}
                                         >
                                         </Form.Control>
                                     </Form.Group>
@@ -135,18 +136,6 @@ export class ModalForm extends Component {
                                         >
                                         </Form.Control>
                                     </Form.Group>
-
-                                    <Form.Group as={Col} controlId="Torre">
-                                        <Form.Label>Torre</Form.Label>
-
-                                        <Form.Control
-                                            type="text"
-                                            name="torre"
-                                            required
-                                            defaultValue={model.dominio}
-                                        >
-                                        </Form.Control>
-                                    </Form.Group>
                                 </Form.Row>
 
                                 <Form.Row>
@@ -160,8 +149,113 @@ export class ModalForm extends Component {
                                 </Form.Row>
                             </Form>
                         </Tab>
-                        <Tab eventKey="technical" title="Ficha técnica">
 
+                        <Tab eventKey="technical" title="Ficha técnica">
+                            <Form>
+                                <Form.Row>
+                                    <Form.Group as={Col} controlId="Ip">
+                                        <Form.Label>IP</Form.Label>
+
+                                        <Form.Control
+                                            type="text"
+                                            name="ip"
+                                            required
+                                            defaultValue={model.ip}
+                                        >
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} controlId="numSérie">
+                                        <Form.Label>Nº de Série</Form.Label>
+
+                                        <Form.Control
+                                            type="text"
+                                            name="numSerie"
+                                            required
+                                            defaultValue={model.numSerie}
+                                        >
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Form.Row>
+
+                                <Form.Row>
+                                    <Form.Group as={Col} controlId="Fabricante">
+                                        <Form.Label>Fabricante</Form.Label>
+
+                                        <Form.Control
+                                            type="text"
+                                            name="fabricante"
+                                            required
+                                            defaultValue={model.fabricante}
+                                        >
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} controlId="Modelo">
+                                        <Form.Label>Modelo</Form.Label>
+
+                                        <Form.Control
+                                            type="text"
+                                            name="modelo"
+                                            required
+                                            defaultValue={model.modelo}
+                                        >
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Form.Row>
+
+                                <Form.Row>
+                                    <Form.Group as={Col} controlId="VersaoOS">
+                                        <Form.Label>Sistema Operacional</Form.Label>
+
+                                        <Form.Control
+                                            type="text"
+                                            name="versaoOS"
+                                            required
+                                            defaultValue={model.versaoOS}
+                                        >
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} controlId="chaveWindows">
+                                        <Form.Label>Chave Windows</Form.Label>
+
+                                        <Form.Control
+                                            type="text"
+                                            name="chaveWindows"
+                                            required
+                                            defaultValue={model.chaveWindows}
+                                        >
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Form.Row>
+
+                                <Form.Row>
+                                    <Form.Group as={Col} controlId="VersaoOffice">
+                                        <Form.Label>Versão Office</Form.Label>
+
+                                        <Form.Control
+                                            type="text"
+                                            name="versaoOffice"
+                                            required
+                                            defaultValue={model.versaoOffice}
+                                        >
+                                        </Form.Control>
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} controlId="chaveOffice">
+                                        <Form.Label>Chave Office</Form.Label>
+
+                                        <Form.Control
+                                            type="text"
+                                            name="chaveOffice"
+                                            required
+                                            defaultValue={model.chaveOffice}
+                                        >
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Form.Row>
+                            </Form>
                         </Tab>
                     </Tabs>
                 </Modal.Body>
