6b5a231ec2785403e0c3ff344b767613fdce0df8
Leonardo Garruba <leonardogarrubaaraujo@gmail.com>
2020-09-16 16:48:04 -0300

    Protegendo as rotas

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './services/auth'
 
 import Login from './pages/login';
 import Computers from './pages/computers'
 import Register from './pages/register'
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props => 
+            isAuthenticated() ? (
+                <Component {...props} />
+            ) : (
+                <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+            )
+        }
+    />
+);
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Login} />
-                <Route path="/computers/main" component={Computers} />
-                <Route path="/computers/register/new" component={Register} />
+                <PrivateRoute path="/computers/main" component={Computers} />
+                <PrivateRoute path="/computers/register/new" component={Register} />
             </Switch>
         </BrowserRouter>
     );
